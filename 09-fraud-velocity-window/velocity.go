b30ff877b335174373c5cb2e34b5990da7aa1b93
Dewa Aditya <dewadityasanjaya@gmail.com>
2026-07-04 23:58:38 +0700

    answer no 9

--- 09-fraud-velocity-window/velocity.go
+++ 09-fraud-velocity-window/velocity.go
@@ -9,8 +9,38 @@ type Transaction struct {
 	CreatedAt   time.Time
 }
 
-func FlagVelocity(transactions []Transaction, window time.Duration, thresholdCents int64) []string {
-	// TODO: implement.
-	return nil
+type cardWindow struct {
+	transactions []Transaction
+	totalCents   int64
 }
 
+func FlagVelocity(transactions []Transaction, window time.Duration, thresholdCents int64) []string {
+	windows := make(map[string]cardWindow)
+	flagged := []string{}
+
+	for _, tx := range transactions {
+		currentWindow := windows[tx.CardID]
+		cutoff := tx.CreatedAt.Add(-window)
+
+		valid := currentWindow.transactions[:0]
+		for _, oldTx := range currentWindow.transactions {
+			if oldTx.CreatedAt.After(cutoff) {
+				valid = append(valid, oldTx)
+			} else {
+				currentWindow.totalCents -= oldTx.AmountCents
+			}
+		}
+
+		currentWindow.transactions = valid
+		currentWindow.transactions = append(currentWindow.transactions, tx)
+		currentWindow.totalCents += tx.AmountCents
+
+		if currentWindow.totalCents > thresholdCents {
+			flagged = append(flagged, tx.ID)
+		}
+
+		windows[tx.CardID] = currentWindow
+	}
+
+	return flagged
+}
